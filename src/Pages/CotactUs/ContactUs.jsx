6dd8ca96ca2a9ac09759723e401d2497c675c790
Faisal Hasan Emon Bhuiyan <faisalhasanemon>
2025-10-18 10:48:22 +0600

    contact information page updated and new icons added

--- src/Pages/CotactUs/ContactUs.jsx
+++ src/Pages/CotactUs/ContactUs.jsx
@@ -1,3 +1,9 @@
+import { Mail, Phone } from "lucide-react";
+import facebook from "../../assets/contactInformationImages/facebook.png";
+import pinterest from "../../assets/contactInformationImages/pinterest.png";
+import instagram from "../../assets/contactInformationImages/instagram.png";
+import twitter from "../../assets/contactInformationImages/twitter.png";
+
 const ContactUs = () => {
   return (
     <div>
@@ -17,25 +23,60 @@ const ContactUs = () => {
 
       {/* Offices Section */}
       <div className="bg-[#FFFFFF]  py-16 px-6">
-        <div className="container px-5 mx-auto  rounded-xl shadow-[0_0_50px_#00000014] p-10 pb-6">
-          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-8">
-            Worldwide Offices
+        <div className="container p-4 md:p-6 mx-auto  rounded-xl shadow-[0_0_50px_#00000014] ">
+          <h2 className="text-2xl text-center md:text-left md:text-4xl font-bold mb-6">
+            Contact Information
           </h2>
-          {/* Grid */}
-          <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            {[...Array(6)].map((_, i) => (
-              <div
-                key={i}
-                className="rounded-lg p-6 shadow-[0_0_20px_rgba(0,0,0,0.10)] bg-white"
-              >
-                <h3 className="font-semibold mb-2">Corporate Headquarters</h3>
-                <p>Savvy Tours</p>
-                <p>example@gmail.com</p>
-                <p>000-000-000</p>
-                <p>205 Babcich St., 12th Floor</p>
-                <p>Westin Palm Beach, FL 33431 US</p>
+          {/* Content */}
+          <div className="px-3 md:px-6 shadow-xl rounded-2xl bg-white">
+            {/* Card 1 - Customer Support */}
+            <div className="border-b-[1px] border-[#CACACA] py-6 space-y-3">
+              {/* Title */}
+              <p className="text-[16px] font-medium text-black">
+                Customer Support
+              </p>
+              {/* Details */}
+              <div className="*:text-[14px] text-[#4A4A4A]">
+                <p>Available Monday - Friday, 9am - 6pm EST</p>
+                <p className="flex justify-start items-center gap-2">
+                  <span>
+                    <Phone size={16} className="text-[#00BF63]" />
+                  </span>{" "}
+                  000-0000-000
+                </p>
+                <p className="flex justify-start items-center gap-2">
+                  <span>
+                    <Mail size={16} className="text-[#00BF63]" />
+                  </span>
+                  example@gmail.com
+                </p>
               </div>
-            ))}
+            </div>
+            {/* Card 2 - Office Address */}
+            <div className="border-b-[1px] border-[#CACACA] py-6 space-y-3">
+              {/* Title */}
+              <p className="text-[16px] font-medium text-black">
+                Customer Support
+              </p>
+              {/* Details */}
+              <div className="*:text-[14px] text-[#4A4A4A]">
+                <p>Available Monday - Friday, 9am - 6pm EST</p>
+                <p>000-0000-000</p>
+                <p>example@gmail.com</p>
+              </div>
+            </div>
+            {/* Card 3 - Social Media */}
+            <div className=" py-6 space-y-3">
+              {/* Title */}
+              <p className="text-[16px] font-medium text-black">Social Media</p>
+              {/* Details */}
+              <div className="flex justify-start items-center gap-6 *:w-6">
+                <img src={facebook} alt="Facebook Logo" />
+                <img src={pinterest} alt="Pinterest" />
+                <img src={twitter} alt="Twitter" />
+                <img src={instagram} alt="Instagram" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
